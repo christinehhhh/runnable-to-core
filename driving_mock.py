f994f5c922eda3c417a3c3743ba5eb07c581e32c
Xinyuuu <xinyu.zhang@tum.de>
2025-05-15 16:06:53 +0200

    Use realistic execution time. Consider delay for executing a runnable while waiting for the previous runnable on the list to be executed

--- driving_mock.py
+++ driving_mock.py
@@ -18,28 +18,28 @@ runnables = {
         'criticality': 0,
         'affinity': 0,
         'period': 50,
-        'execution_time': 4,
+        'execution_time': 7,
         'type': 'periodic',
         'deps': []
     },
     'SensorFusion': {
         'criticality': 1,
         'affinity': 1,
-        'execution_time': 5,
+        'execution_time': 6,
         'type': 'event',
         'deps': ['RadarCapture', 'CameraCapture'],
     },
     'ObjectDetection': {
         'criticality': 1,
         'affinity': 1,
-        'execution_time': 6,
+        'execution_time': 15,
         'type': 'event',
         'deps': ['SensorFusion'],
     },
     'TrajectoryPrediction': {
         'criticality': 1,
         'affinity': 1,
-        'execution_time': 4,
+        'execution_time': 8,
         'type': 'event',
         'deps': ['ObjectDetection'],
     },
@@ -67,14 +67,14 @@ runnables = {
     'LaneMarkingDetection': {
         'criticality': 1,
         'affinity': 1,
-        'execution_time': 5,
+        'execution_time': 6,
         'type': 'event',
         'deps': ['CameraCapture'],
     },
     'VehiclePositionEstimation': {
         'criticality': 1,
         'affinity': 1,
-        'execution_time': 3,
+        'execution_time': 4,
         'type': 'event',
         'deps': ['LaneMarkingDetection'],
     },
@@ -107,7 +107,7 @@ heapq.heapify(event_queue)
 last_output = defaultdict(lambda: -1)
 execution_log = []
 
-cpu_free_time = 0
+CPU_FREE_TIME = 0
 
 
 def schedule_periodic_runnables():
@@ -117,7 +117,8 @@ def schedule_periodic_runnables():
         if props['type'] == 'periodic':
             time = 0
             while time <= SIMULATION_TIME_MS:
-                heapq.heappush(event_queue, (time, 0, name))
+                heapq.heappush(
+                    event_queue, (time, 0, name, props['execution_time']))
                 time += props['period']
 
 
@@ -132,18 +133,21 @@ def schedule_event_runnables(triggered, current_time):
     for name, props in runnables.items():
         if props['type'] == 'event' and set(props.get('deps', [])) & set(triggered):
             if is_dependencies_ready(name, current_time):
-                heapq.heappush(event_queue, (current_time, 1, name))
+                total_delay = sum(exec_time for sched_time, _, _, exec_time in event_queue
+                                  if sched_time < current_time)
+                heapq.heappush(
+                    event_queue, (current_time + total_delay, 1, name, props['execution_time']))
 
 
 schedule_periodic_runnables()
 
-while event_queue:
-    scheduled_time, priority, task = heapq.heappop(event_queue)
+while event_queue and CPU_FREE_TIME < SIMULATION_TIME_MS:
+    scheduled_time, priority, task, _ = heapq.heappop(event_queue)
 
-    actual_start_time = max(cpu_free_time, scheduled_time)
+    actual_start_time = max(CPU_FREE_TIME, scheduled_time)
     execution_time = runnables[task]['execution_time']
     finish_time = actual_start_time + execution_time
-    cpu_free_time = finish_time
+    CPU_FREE_TIME = finish_time
 
     last_output[task] = finish_time
     execution_log.append((actual_start_time, finish_time, task))
