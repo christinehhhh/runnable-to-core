67b53c6a0057a399eb5d89a345542a7f015ad694
Xinyuuu <xinyu.zhang@tum.de>
2025-07-07 09:04:56 +0200

    Add 0 - QM to criticality options

--- frontend/app/_components/RunnableConfigPanel.tsx
+++ frontend/app/_components/RunnableConfigPanel.tsx
@@ -76,10 +76,11 @@ const RunnableConfigPanel = () => {
   }
 
   const criticalityOptions = [
-    { value: '0', label: '0 - ASIL A' },
-    { value: '1', label: '1 - ASIL B' },
-    { value: '2', label: '2 - ASIL C' },
-    { value: '3', label: '3 - ASIL D' },
+    { value: '0', label: '0 - QM' },
+    { value: '1', label: '1 - ASIL A' },
+    { value: '2', label: '2 - ASIL B' },
+    { value: '3', label: '3 - ASIL C' },
+    { value: '4', label: '4 - ASIL D' },
   ]
 
   const affinityOptions = Array.from({ length: numCores }, (_, i) => ({
