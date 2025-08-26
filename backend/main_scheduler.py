83507f4c733f218a275f97051c9391461d4f98e6
Xinyuuu <xinyu.zhang@tum.de>
2025-08-26 15:44:31 +0200

    Remove new runnables

--- backend/main_scheduler.py
+++ backend/main_scheduler.py
@@ -447,36 +447,36 @@ runnables = {
         'deps': ['LaneDepartureWarning', 'SteeringAngleCalculation'],
     },
 
-    'DistanceEstimation': {
-        'priority': 1,
-        'execution_time': 5,
-        'type': 'event',
-        'deps': ['ObjectDetection'],
-    },
-    'RelativeSpeedEstimation': {
-        'priority': 1,
-        'execution_time': 4,
-        'type': 'event',
-        'deps': ['ObjectDetection'],
-    },
-    'AdaptiveCruiseControlDecision': {
-        'priority': 2,
-        'execution_time': 6,
-        'type': 'event',
-        'deps': ['DistanceEstimation', 'RelativeSpeedEstimation'],
-    },
-    'ThrottleControl': {
-        'priority': 2,
-        'execution_time': 2,
-        'type': 'event',
-        'deps': ['AdaptiveCruiseControlDecision'],
-    },
-    'BrakeControl': {
-        'priority': 2,
-        'execution_time': 2,
-        'type': 'event',
-        'deps': ['AdaptiveCruiseControlDecision'],
-    },
+    # 'DistanceEstimation': {
+    #     'priority': 1,
+    #     'execution_time': 5,
+    #     'type': 'event',
+    #     'deps': ['ObjectDetection'],
+    # },
+    # 'RelativeSpeedEstimation': {
+    #     'priority': 1,
+    #     'execution_time': 4,
+    #     'type': 'event',
+    #     'deps': ['ObjectDetection'],
+    # },
+    # 'AdaptiveCruiseControlDecision': {
+    #     'priority': 2,
+    #     'execution_time': 6,
+    #     'type': 'event',
+    #     'deps': ['DistanceEstimation', 'RelativeSpeedEstimation'],
+    # },
+    # 'ThrottleControl': {
+    #     'priority': 2,
+    #     'execution_time': 2,
+    #     'type': 'event',
+    #     'deps': ['AdaptiveCruiseControlDecision'],
+    # },
+    # 'BrakeControl': {
+    #     'priority': 2,
+    #     'execution_time': 2,
+    #     'type': 'event',
+    #     'deps': ['AdaptiveCruiseControlDecision'],
+    # },
 }
 
 # Re-run
