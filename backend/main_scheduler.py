3e386a4e1330a3d82a8c210e0ce51c3cc5fe9e14
Xinyuuu <xinyu.zhang@tum.de>
2025-08-26 16:12:39 +0200

    Update priority for some runnables

--- backend/main_scheduler.py
+++ backend/main_scheduler.py
@@ -481,52 +481,52 @@ runnables_long_path = {
     'Task2':  {'priority': 2, 'execution_time': 20, 'type': 'periodic', 'period': 150, 'deps': []},
 
     'Task3':  {'priority': 1, 'execution_time': 25, 'type': 'event', 'deps': ['Task1']},
-    'Task4':  {'priority': 1, 'execution_time': 30, 'type': 'event', 'deps': ['Task3']},
-    'Task5':  {'priority': 1, 'execution_time': 20, 'type': 'event', 'deps': ['Task4']},
+    'Task4':  {'priority': 4, 'execution_time': 30, 'type': 'event', 'deps': ['Task3']},
+    'Task5':  {'priority': 3, 'execution_time': 20, 'type': 'event', 'deps': ['Task4']},
     'Task6':  {'priority': 1, 'execution_time': 35, 'type': 'event', 'deps': ['Task5']},
     'Task7':  {'priority': 2, 'execution_time': 40, 'type': 'event', 'deps': ['Task6']},
-    'Task8':  {'priority': 2, 'execution_time': 25, 'type': 'event', 'deps': ['Task7']},
-    'Task9':  {'priority': 2, 'execution_time': 30, 'type': 'event', 'deps': ['Task8']},
-    'Task10': {'priority': 2, 'execution_time': 20, 'type': 'event', 'deps': ['Task9']},
+    'Task8':  {'priority': 1, 'execution_time': 25, 'type': 'event', 'deps': ['Task7']},
+    'Task9':  {'priority': 0, 'execution_time': 30, 'type': 'event', 'deps': ['Task8']},
+    'Task10': {'priority': 4, 'execution_time': 20, 'type': 'event', 'deps': ['Task9']},
     'Task11': {'priority': 2, 'execution_time': 45, 'type': 'event', 'deps': ['Task10']},
-    'Task12': {'priority': 3, 'execution_time': 30, 'type': 'event', 'deps': ['Task11']},
+    'Task12': {'priority': 0, 'execution_time': 30, 'type': 'event', 'deps': ['Task11']},
     'Task13': {'priority': 3, 'execution_time': 35, 'type': 'event', 'deps': ['Task12']},
-    'Task14': {'priority': 3, 'execution_time': 25, 'type': 'event', 'deps': ['Task13']},
+    'Task14': {'priority': 1, 'execution_time': 25, 'type': 'event', 'deps': ['Task13']},
     'Task15': {'priority': 3, 'execution_time': 40, 'type': 'event', 'deps': ['Task14']},
     'Task16': {'priority': 3, 'execution_time': 20, 'type': 'event', 'deps': ['Task15']},
     'Task17': {'priority': 4, 'execution_time': 50, 'type': 'event', 'deps': ['Task16']},
-    'Task18': {'priority': 4, 'execution_time': 25, 'type': 'event', 'deps': ['Task17']},
+    'Task18': {'priority': 1, 'execution_time': 25, 'type': 'event', 'deps': ['Task17']},
     'Task19': {'priority': 4, 'execution_time': 35, 'type': 'event', 'deps': ['Task18']},
-    'Task20': {'priority': 4, 'execution_time': 30, 'type': 'event', 'deps': ['Task19']},
+    'Task20': {'priority': 2, 'execution_time': 30, 'type': 'event', 'deps': ['Task19']},
 }
 
 runnables_balanced = {
     'Task1':  {'priority': 1, 'execution_time': 20, 'type': 'periodic', 'period': 100, 'deps': []},
-    'Task2':  {'priority': 1, 'execution_time': 25, 'type': 'periodic', 'period': 180, 'deps': []},
+    'Task2':  {'priority': 3, 'execution_time': 25, 'type': 'periodic', 'period': 180, 'deps': []},
 
-    'Task3':  {'priority': 1, 'execution_time': 30, 'type': 'event', 'deps': ['Task1']},
-    'Task4':  {'priority': 1, 'execution_time': 20, 'type': 'event', 'deps': ['Task1']},
+    'Task3':  {'priority': 0, 'execution_time': 30, 'type': 'event', 'deps': ['Task1']},
+    'Task4':  {'priority': 4, 'execution_time': 20, 'type': 'event', 'deps': ['Task1']},
     'Task5':  {'priority': 2, 'execution_time': 40, 'type': 'event', 'deps': ['Task2']},
     'Task6':  {'priority': 2, 'execution_time': 30, 'type': 'event', 'deps': ['Task2']},
 
     'Task7':  {'priority': 2, 'execution_time': 25, 'type': 'event', 'deps': ['Task3', 'Task4']},
     'Task8':  {'priority': 2, 'execution_time': 35, 'type': 'event', 'deps': ['Task5', 'Task6']},
-    'Task9':  {'priority': 2, 'execution_time': 20, 'type': 'event', 'deps': ['Task3']},
+    'Task9':  {'priority': 4, 'execution_time': 20, 'type': 'event', 'deps': ['Task3']},
     'Task10': {'priority': 2, 'execution_time': 30, 'type': 'event', 'deps': ['Task4']},
 
-    'Task11': {'priority': 3, 'execution_time': 40, 'type': 'event', 'deps': ['Task7']},
+    'Task11': {'priority': 1, 'execution_time': 40, 'type': 'event', 'deps': ['Task7']},
     'Task12': {'priority': 3, 'execution_time': 25, 'type': 'event', 'deps': ['Task8']},
-    'Task13': {'priority': 3, 'execution_time': 35, 'type': 'event', 'deps': ['Task9', 'Task10']},
+    'Task13': {'priority': 0, 'execution_time': 35, 'type': 'event', 'deps': ['Task9', 'Task10']},
 
-    'Task14': {'priority': 3, 'execution_time': 30, 'type': 'event', 'deps': ['Task11']},
+    'Task14': {'priority': 2, 'execution_time': 30, 'type': 'event', 'deps': ['Task11']},
     'Task15': {'priority': 3, 'execution_time': 20, 'type': 'event', 'deps': ['Task12']},
-    'Task16': {'priority': 3, 'execution_time': 40, 'type': 'event', 'deps': ['Task13']},
+    'Task16': {'priority': 4, 'execution_time': 40, 'type': 'event', 'deps': ['Task13']},
 
-    'Task17': {'priority': 4, 'execution_time': 45, 'type': 'event', 'deps': ['Task14', 'Task15']},
-    'Task18': {'priority': 4, 'execution_time': 30, 'type': 'event', 'deps': ['Task16']},
+    'Task17': {'priority': 1, 'execution_time': 45, 'type': 'event', 'deps': ['Task14', 'Task15']},
+    'Task18': {'priority': 0, 'execution_time': 30, 'type': 'event', 'deps': ['Task16']},
 
-    'Task19': {'priority': 4, 'execution_time': 50, 'type': 'event', 'deps': ['Task17', 'Task18']},
-    'Task20': {'priority': 4, 'execution_time': 25, 'type': 'event', 'deps': ['Task19']},
+    'Task19': {'priority': 2, 'execution_time': 50, 'type': 'event', 'deps': ['Task17', 'Task18']},
+    'Task20': {'priority': 1, 'execution_time': 25, 'type': 'event', 'deps': ['Task19']},
 }
 
 
